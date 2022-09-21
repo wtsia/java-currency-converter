9017811ca6e0e15fa1e6c1066f4c7730a7e233c6
Winston <winstontsia@gmail.com>
2022-09-21 01:07:21 -0700

    initializing public unit conversion variables in Conversion.java

--- Conversion.java
+++ Conversion.java
@@ -1,6 +1,15 @@
 import java.util.Scanner
 
 public class Conversion {
+	public static double unitUsdToRmb = 7.05;
+	public static double unitRmbToUsd = 0.14;
+	
+	/**
+	 * Main method class
+	 * @parameters
+	 * @return
+	 *
+	 */
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 
