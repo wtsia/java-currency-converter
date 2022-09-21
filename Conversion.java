35e27b1db83afccb0ef83f81542b4e15e5aec7cd
Winston <winstontsia@gmail.com>
2022-09-21 00:07:50 -0700

    initalizing Conversion.java

---
+++ Conversion.java
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+public class Conversion {
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+
+		// comment
+		//
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # java-currency-converter
 program that converts currency (from USD to Euro, vice versa)
+
+menu
+
+main
+
+currency method 
+
+currency method
+
+
