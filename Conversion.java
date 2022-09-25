b3d2782e2915e04c021ad023444ce5ab8aef80b6
Winston <winstontsia@gmail.com>
2022-09-25 14:42:38 -0700

    cleaned up code, added switch in main method to identify conversionType

--- Conversion.java
+++ Conversion.java
@@ -12,11 +12,14 @@ class Conversion {
 	/**
 	 * Method to convert USD to RMB
 	 * Takes in an input and returns the unit conversion of USD to ROMB
-	 * @parameters input as a double
+	 * @parameters double input
 	 * @return input*unitUsdToRmb
 	 */
 	public static double convertUsdToRmb (double input) {
 		return input*unitUsdToRmb;
+	}
+	public static double convertRmbToUsd (double input) {
+		return input*unitRmbToUsd;
 	}
 	
 	/**
@@ -30,56 +33,40 @@ class Conversion {
 		System.out.println("****************************************************");
 		System.out.println("*            CURRENCY CONVERSION TOOL              *");
 		System.out.println("*                                                  *");
-		System.out.println("*                   AUTHORS:                       *");
+		System.out.println("*                    AUTHORS:                      *");
 		System.out.println("*             Higino L.P., Winston T.              *");
 		System.out.println("* https://github.com/wtsia/java-currency-converter *");
 		System.out.println("*                                                  *");
 		System.out.println("*             Choose Conversion Type:              *");
 		System.out.println("*                                                  *");
 		System.out.println("* 1. United States Dollar (USD) to Renminbu (RMB)  *");
-		System.out.println("* 2. Renminbu (RMB) to United States Dollar (USD)  *");
+		System.out.println("* 2. Renminbi (RMB) to United States Dollar (USD)  *");
 		System.out.println("* 3. Exit                                          *");
 		System.out.println("*         (enter conversion type below)            *");
-		System.out.println("****************************************************");
-		// 
-		//
+		System.out.println("****************************************************\n");
 		
-		int conversionType = in.nextInt();
+		System.out.print("Your answer:");
+		// int conversionType = in.nextInt();
 		// Print out for testing
 		// System.out.println(conversionType);
-		return conversionType;
-	}
+		return in.nextInt();
+	};
 	
-//	/**
-//	 * Main method class
-//	 * @parameters
-//	 * @return
-//	 *
-//	 */
-//	public static void main(String[] args) {
-//		Scanner scnr = new Scanner(System.in);
-//
-//		// comment
-//		//
-//	}
-
+	/**
+	 * Main method class
+	 * @parameters
+	 * @return
+	 *
+	 */
 	public static void main(String[] args) {
-		//scanner for input of conversion desired
-//		System.out.println("Which option would you like to do:");
-//		System.out.println("1.convert USD to RMB");
-//		System.out.println("2.Convert RMB to USD");
-//		System.out.println("3.Exit");
+		int conversionType = presentMenu();
 		
-		if (presentMenu() == 3) {
-			System.out.println("goodbye!");
+		if (conversionType == 3) {
+			System.out.println("Exiting the tool... goodbye!");
 			System.exit(0);
-		};
-//		Scanner menuInput = new Scanner(System.in);
-//		
-//		System.out.println("Please input an option number");
-//		
-//		String optionNum = menuInput.nextLine();
-//		System.out.println("You selected option " + optionNum + " ."); 
+		} else {
+			System.out.println(conversionType + " is the chosen type");
+		}
 	}
 }
 
